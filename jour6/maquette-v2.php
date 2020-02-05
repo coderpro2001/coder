8389321a88e790af1e77c1e387afdd6e72808609
LH <github@applh.com>
2020-02-05 11:46:51 +0100

    ajout maquette

---
+++ jour6/maquette-v2.php
@@ -0,0 +1,6 @@
+<?php
+
+// COMPOSITION DE PLUSIEURS FICHIERS PHP
+require_once "php/view/header.php";
+require_once "php/view/section-index.php";
+require_once "php/view/footer.php";
---
+++ jour6/maquette.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>titre</h1>
+        <nav>
+            <!-- ATTENTION: NE PAS OUBLIER LES LIENS EN HTML -->
+            <a href="index.php">accueil</a>
+            <a href="services.php">services</a>
+            <a href="contact.php">contact</a>
+        </nav>
+    </header>
+    <main>
+        <!-- DECOUPAGE EN PHP -->
+        <section>
+            <h2>titre de la section</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque illo incidunt corrupti itaque, aperiam hic, quas quia pariatur quos obcaecati beatae debitis culpa doloribus, sapiente cum voluptatem sint officiis.</p>
+            <img src="assets/img/fleur.jpg" alt="fleur">
+        </section>
+        <!-- DECOUPAGE EN PHP -->
+    </main>
+    <footer>
+        <p>tous droits réservés</p>
+    </footer>
+</body>
+</html>
