4c4e0ab51707ade597b034492cd1afa778a40934
LH <github@applh.com>
2020-03-02 12:24:19 +0100

    ajout js et css

--- vitrine/assets/js/app.js
+++ vitrine/assets/js/app.js
@@ -1,1 +1,38 @@
 console.log("JS CHARGE");
+
+// JE SELECTIONNE LES BALISES img QUI SONT ENFANTS DE .container
+// ET JE MEMORISE LA LISTE DANS LA VARIABLE listeImg
+var listeImg = document.querySelectorAll(".container img");
+
+console.log(listeImg);
+
+// SUR LA LISTE DES IMAGES, 
+// JE VEUX AJOUTER UNE INTERACTION QUAND LE VISITEUR CLIQUE SUR UNE IMAGE
+// ON FAIT UNE BOUCLE
+for(var i=0; i < listeImg.length; i++)
+{
+    // ON UTILISE L'INDICE POUR ACCEDER A UN ELELENT DU TABLEAU
+    var imageCourante = listeImg[i];
+    // JE VEUX QUE QUAND LE VISITEUR CLIQUE SUR UNE IMAGE
+    // JS VA ACTIVER LE CODE DANS LA FONCTION
+    imageCourante.addEventListener("click", function(event){
+        console.log("LE VISITEUR A CLIQUE");
+        // SI JE VEUX SAVOIR SUR QUELLE IMAGE LE VISITEUR A CLIQUE
+        console.log(event.target);
+        // JE VEUX CHANGER L'IMAGE EN GRAND ET LA REMPLACER PAR L'IMAGE CLIQUEE
+        // => INTERACTION AVEC LE VISITEUR
+        // EN HTML, POUR CHANGER L'IMAGE AFFICHEE 
+        // => IL FAUT CHANGER LA VALEUR DE L'ATTRIBUT src
+
+        // AU DEBUT, J'AI BESOIN DE RECUPERER LA VALEUR DE L'ATTRIBUT src 
+        // DE L'IMAGE CLIQUEE
+        var baliseCliquee = event.target;
+        // JE LIS LA VALEUR D'UN ATTRIBUT
+        var urlImageCliquee = baliseCliquee.getAttribute("src");
+
+        // POUR SELECTIONNER EN JS LA BALISE img QUE JE VEUX
+        var balisePrincipale = document.querySelector(".imagePrincipale");
+        // SI JE VEUX CHANGER L'ATTRIBUT D'UNE BALISE
+        balisePrincipale.setAttribute("src", urlImageCliquee);
+    });
+}
--- vitrine/php/view/section-catalogue.php
+++ vitrine/php/view/section-catalogue.php
@@ -2,7 +2,31 @@
         <section>
             <h2>catalogue</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eos qui ratione dolorum ut corporis repudiandae rerum expedita harum nobis ad voluptatibus, obcaecati sequi. Dolore hic distinctio eveniet molestiae incidunt!</p>
-            <img src="assets/img/voiture.jpg" alt="voiture">
+            <!-- JE RAJOUTE UNE CLASSE POUR POUVOIR FACILEMENT SELECTIONNER LA BALISE EN JS -->
+            <img class="imagePrincipale" src="assets/img/galerie1.jpg" alt="voiture">
+            <div class="container">
+<?php
+
+// JE DEMANDE A PHP DE RECUPERER LA LISTE DES FICHIERS QUI COMMENCE PAR galerie
+
+$listeGalerie = glob("assets/img/galerie*.jpg");
+foreach($listeGalerie as $image)
+{
+    echo 
+<<<CODEHTML
+
+    <img src="$image" alt="$image">
+
+CODEHTML;
+}
+
+?>
+<!--                
+                <img src="assets/img/voiture1.jpg" alt="voiture1">
+                <img src="assets/img/voiture2.jpg" alt="voiture2">
+                <img src="assets/img/voiture3.jpg" alt="voiture3">
+-->                
+            </div>
         </section>
 
         
