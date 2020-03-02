9a4f6b306fff89034c1e4e02874eaf8dedfde5e3
LH <github@applh.com>
2020-03-02 15:11:06 +0100

    debug

--- vitrine/assets/js/app.js
+++ vitrine/assets/js/app.js
@@ -4,7 +4,7 @@ console.log("JS CHARGE");
 // ET JE MEMORISE LA LISTE DANS LA VARIABLE listeImg
 var listeImg = document.querySelectorAll(".container img");
 
-console.log(listeImg);
+// console.log(listeImg);
 
 // SUR LA LISTE DES IMAGES, 
 // JE VEUX AJOUTER UNE INTERACTION QUAND LE VISITEUR CLIQUE SUR UNE IMAGE
@@ -17,8 +17,10 @@ for(var i=0; i < listeImg.length; i++)
     // JS VA ACTIVER LE CODE DANS LA FONCTION
     imageCourante.addEventListener("click", function(event){
         console.log("LE VISITEUR A CLIQUE");
+
         // SI JE VEUX SAVOIR SUR QUELLE IMAGE LE VISITEUR A CLIQUE
         console.log(event.target);
+
         // JE VEUX CHANGER L'IMAGE EN GRAND ET LA REMPLACER PAR L'IMAGE CLIQUEE
         // => INTERACTION AVEC LE VISITEUR
         // EN HTML, POUR CHANGER L'IMAGE AFFICHEE 
@@ -27,11 +29,13 @@ for(var i=0; i < listeImg.length; i++)
         // AU DEBUT, J'AI BESOIN DE RECUPERER LA VALEUR DE L'ATTRIBUT src 
         // DE L'IMAGE CLIQUEE
         var baliseCliquee = event.target;
+
         // JE LIS LA VALEUR D'UN ATTRIBUT
         var urlImageCliquee = baliseCliquee.getAttribute("src");
 
         // POUR SELECTIONNER EN JS LA BALISE img QUE JE VEUX
         var balisePrincipale = document.querySelector(".imagePrincipale");
+        
         // SI JE VEUX CHANGER L'ATTRIBUT D'UNE BALISE
         balisePrincipale.setAttribute("src", urlImageCliquee);
     });
