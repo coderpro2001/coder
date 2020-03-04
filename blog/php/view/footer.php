3f28a10e860f2340c84abe67c54a662a7948db39
LH <github@applh.com>
2020-03-04 10:42:00 +0100

    ajout page admin

--- blog/php/view/footer.php
+++ blog/php/view/footer.php
@@ -2,6 +2,8 @@
 </main>
     <footer>
         <p>tous droits réservés</p>
+        <!-- POUR ALLER SUR LA PAGE ADMIN -->
+        <a href="admin.php">ADMIN</a>
     </footer>
     <script src="assets/js/app.js"></script>
 </body>
---
+++ blog/php/view/section-admin.php
@@ -0,0 +1,56 @@
+
+<section>
+    <h2>FORMULAIRE DE CREATION D'ARTICLE</h2>
+    <form class="admin" action="" method="POST">
+        <input type="text" name="titre" required placeholder="entrer le titre">
+        <textarea name="contenu" cols="60" rows="8" required placeholder="entrer le contenu"></textarea>
+        <!-- POUR L'IMAGE, ON DEVRAIT PROPOSER UN UPLOAD => PLUS TARD... -->
+        <input type="text" name="image" required value="assets/img/photo1.jpg">
+        <!-- https://www.php.net/manual/fr/function.date.php -->
+        <input type="text" name="datePublication" value="<?php echo date("Y-m-d H:i:s") ?>">
+        <input type="text" name="categorie" required placeholder="entrez la catégorie">
+        <button type="submit">PUBLIER L'ARTICLE</button>
+        <div class="confirmation">
+<?php
+// TRAITEMENT DU FORMULAIRE DE CREATION D'ARTICLE
+if (count($_REQUEST) > 0)
+{
+    // DEBUG
+    // echo "JE DOIS TRAITER LE FORMULAIRE";
+
+    // $_REQUEST EST UN TABLEAU ASSOCIATIF
+
+    // ETAPE1: RECUPERER LES INFOS DU FORMULAIRE
+    $titre           = $_REQUEST["titre"];
+    $contenu         = $_REQUEST["contenu"];
+    $image           = $_REQUEST["image"];
+    $datePublication = $_REQUEST["datePublication"];
+    $categorie       = $_REQUEST["categorie"];
+
+    // ETAPE2: ON VA CONSTRUIRE LA REQUETE SQL INSERT
+    $requeteSQL   =
+<<<CODESQL
+
+INSERT INTO articles
+( titre, contenu, image, datePublication, categorie)
+VALUES
+( '$titre', '$contenu', '$image', '$datePublication', '$categorie') 
+
+CODESQL;
+
+
+    // ETAPE3: ON VA ENVOYER LA REQUETE SQL 
+    // POUR INSERER UNE NOUVELLE LIGNE DANS LA TABLE artciles
+    // CONNEXION A LA LA DATABASE
+    $pdo = new PDO("mysql:host=localhost;dbname=blog;charset=utf8", "root", "");
+    // ENVOYER LA REQUETE
+    $pdo->exec($requeteSQL);
+
+    // MESSAGE DE CONFRFIRMATION
+    echo "VOTRE ARTICLE A ETE PUBLIE";
+}
+
+?>
+        </div>
+    </form>
+</section>
