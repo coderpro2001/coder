ab00682ffec51ac224484118d6807ad74de8df07
LH <github@applh.com>
2020-03-04 12:28:54 +0100

    ajout code sql create table contact

--- blog/php/model/blog.sql
+++ blog/php/model/blog.sql
@@ -37,35 +37,33 @@ CREATE TABLE `articles` (
   `categorie` varchar(160) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Déchargement des données de la table `articles`
---
-
-INSERT INTO `articles` (`id`, `titre`, `contenu`, `image`, `datePublication`, `categorie`) VALUES
-(1, 'coronavirus', 'sera-t-il bloqué par les frontières ?', 'assets/img/photo1.jpg', '2020-03-03 00:00:00', 'sante'),
-(2, 'benjamin griveaux', 'il est passé où le benjamin ?', '/assets/img/photo2.jpg', '2020-03-03 00:00:00', 'politique');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `articles`
---
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `articles`
---
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+/*
+SI ON VEUT CREER UNE TABLE AVEC DU CODE SQL
+ET NE PAS PASSER PAR PHPMYADMIN
+*/ 
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(160) NOT NULL,
+  `email` varchar(160) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
