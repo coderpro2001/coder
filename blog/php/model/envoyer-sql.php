d4457071f78940a86c343be9e0887bdf6df1d4b5
LH <github@applh.com>
2020-03-04 13:35:02 +0100

    ajout doc

--- blog/php/model/envoyer-sql.php
+++ blog/php/model/envoyer-sql.php
@@ -8,9 +8,12 @@
 
 // ETAPE1: CONNECTER PHP A SQL
 $pdo = new PDO("mysql:host=localhost;dbname=blog;charset=utf8;", "root", "");
-// https://www.php.net/manual/fr/pdo.query.php
+
+// ETAPE2a: ON ENVOIE LA REQUETE PREPAREE
 // PDOStatement EST UN CONTAINER QUI ENGLOBE LES RESULTATS DE LA REQUETE SQL
 $pdoStatement = $pdo->prepare($requeteSQL);
+
+// ETAPE2b: ON FOURNIT LES DONNEES EXTERIEURES A PART
 $pdoStatement->execute($tabAssoColonneValeur);
 
 
