a6d9b228531549587e5bb2e8b97ddf8e457e43d2
LH <github@applh.com>
2020-03-13 15:46:39 +0100

    ajout cellspacing="0"

--- blog/php/view/section-admin.php
+++ blog/php/view/section-admin.php
@@ -51,7 +51,7 @@ if ($identifiantFormulaire == "delete")
 <section>
     <h2>LISTE DES ARTICLES</h2>
 
-    <table>
+    <table cellspacing="0">
         <thead>
             <!-- TITRE DES COLONNES -->
             <!-- LIGNE => TABLE ROW => tr -->
