db745b3dab6fe30542f4ada04e30d56c7fa56776
LH <github@applh.com>
2020-03-13 11:01:03 +0100

    ajout code traitement formulaire

--- blog/php/controller/form-articles.php
+++ blog/php/controller/form-articles.php
@@ -1,8 +1,69 @@
 <?php
 
-// TRAITEMENT DU FORMULAIRE DE CREATION D'ARTICLE
-if (count($_REQUEST) > 0)
+// DANS QUEL CAS, ON PEUT CREER DES FONCTIONS POUR SE SIMPLIFIER LE CODE ?
+// SI ON DETECTE DES BLOCS DE CODE QUI SE REPETENT A PLUSIEURS ENDROITS DIFFERENTS
+// => PENSER A CREER UNE FONCTION
+// TYPIQUEMENT, UNE FONCTION PEUT SERVIR DE FILTRE
+function filtrer($name="id")
 {
+    $resultat = $_REQUEST[$name] ?? "";
+    return $resultat;
+}
+
+
+// JE VAIS RECUPERER L'INFO DE L'IDENTIFIANT DU FORMULAIRE
+//         <input type="hidden" name="identifiantFormulaire" value="delete">
+// ?? "" => SI LE FORMULAIRE N'A RIEN ENVOYE ALORS ON PREND "" COMME VALEUR PAR DEFAUT
+// $identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
+$identifiantFormulaire = filtrer("identifiantFormulaire");
+
+if ($identifiantFormulaire == "delete")
+{
+    // CODE DE TRAITEMENT DU FORMULAIRE DE DELETE
+    // IL FAIT RECUPERER L'INFO DE L'id DE LA LIGNE A SUPPRIMER
+
+    // ET ENSUITE IL FAUT CONSTRUIRE LA REQUETE SQL 
+    // POUR SUPPRIMER LA LIGNE DANS LA TABLE articles
+    // ET ENFIN, ON VA ENVOYER LA REQUETE SQL
+    $tabAssoColonneValeur = [
+        "id"            => filtrer("id"),
+    ];
+    extract($tabAssoColonneValeur);
+    // ON PEUT SE PROTEGER EN PHP
+    // EN CONVERTISSANT EN NOMBRE
+    // https://www.php.net/manual/fr/function.intval.php
+    $id = intval($id);
+
+    if ($id > 0)
+    {
+        // ON NE PREND PAS LES INFOS DU FORMULAIRE POUR CONCATENER ET CREER LA REQUETE SQL
+        // (ATTAQUE PAR INJECTION SQL...)
+        $requeteSQL   =
+<<<CODESQL
+
+DELETE FROM articles
+WHERE id = :id
+
+CODESQL;
+
+        require_once "php/model/envoyer-sql.php";
+
+        // MESSAGE DE CONFIRMATION
+        echo "VOTRE ARTICLE A ETE SUPPRIME ($requeteSQL)";
+
+    }
+    else
+    {
+        // MESSAGE DE CONFIRMATION
+        echo "MERCI DE NE PAS HAKCER MON SITE...";
+    }
+
+}
+
+if ($identifiantFormulaire == "create")
+{
+    // CODE DE TRAITEMENT DU FORMULAIRE DE CREATE
+
     // DEBUG
     // echo "JE DOIS TRAITER LE FORMULAIRE";
 
@@ -33,17 +94,28 @@ if (count($_REQUEST) > 0)
     // "nom de la colonne SQL" => $_REQUEST["attribut name HTML"]
     
     $tabAssoColonneValeur = [
-        "titre"            => $_REQUEST["titre"],
-        "contenu"          => $_REQUEST["contenu"],
-        "image"            => $_REQUEST["image"],
-        "datePublication"  => $_REQUEST["datePublication"],
-        "categorie"        => $_REQUEST["categorie"],
+        "titre"            => filtrer("titre"),
+        "contenu"          => filtrer("contenu"),
+        "image"            => filtrer("image"),
+        "datePublication"  => filtrer("datePublication"),
+        "categorie"        => filtrer("categorie"),
     ];
+    // RACCOURCI POUR CREER LES VARIABLES A PARTIR DU TABLEAU ASSOCIATIF
+    extract($tabAssoColonneValeur);
 
-    // ETAPE2: ON VA CONSTRUIRE LA REQUETE SQL INSERT
-    // POUR SE PROTEGER, ON NE VA PAS CONCATENER AVEC DES ELEMENTS EXTERIEURS
-    // ON PREPARE LE CODE SQL ET PLUS TARD ON VA L'EXECUTER
-    $requeteSQL   =
+    // IL FAUT RAJOUTER DE LA SECURITE
+    // POUR VALIDER QUE TOUTES LES INFOS NECESSAIRES SONT PRESENTES
+    if ($titre != "" 
+            && $contenu != ""
+            && $image != ""
+            && $datePublication != ""
+            && $categorie != "")
+    {
+        // SCENARIO OK
+        // ETAPE2: ON VA CONSTRUIRE LA REQUETE SQL INSERT
+        // POUR SE PROTEGER, ON NE VA PAS CONCATENER AVEC DES ELEMENTS EXTERIEURS
+        // ON PREPARE LE CODE SQL ET PLUS TARD ON VA L'EXECUTER
+        $requeteSQL   =
 <<<CODESQL
 
 INSERT INTO articles
@@ -54,12 +126,20 @@ VALUES
 CODESQL;
 
 
-    // ETAPE3: ON VA ENVOYER LA REQUETE SQL 
-    // JE CHARGE LE CODE PHP POUR ENVOYER LA REQUETE
-    require_once "php/model/envoyer-sql.php";
+        // ETAPE3: ON VA ENVOYER LA REQUETE SQL 
+        // JE CHARGE LE CODE PHP POUR ENVOYER LA REQUETE
+        require_once "php/model/envoyer-sql.php";
 
-    // MESSAGE DE CONFIRMATION
-    echo "VOTRE ARTICLE A ETE PUBLIE ($requeteSQL)";
+        // MESSAGE DE CONFIRMATION
+        echo "VOTRE ARTICLE A ETE PUBLIE ($requeteSQL)";
+    }
+    else
+    {
+        // SCENARIO KO
+        // MESSAGE DE CONFIRMATION
+        echo "VEUILLEZ REMPLIR TOUS LES CHAMPS OBLIGATOIRES";
+    }
+
 }
 
 
--- blog/php/view/section-admin.php
+++ blog/php/view/section-admin.php
@@ -1,7 +1,7 @@
 
 <section>
     <h2>FORMULAIRE DE CREATION D'ARTICLE</h2>
-    <form class="admin" action="" method="POST">
+    <form id="create" class="admin" action="" method="POST">
         <input type="text" name="titre" required placeholder="entrer le titre">
         <textarea name="contenu" cols="60" rows="8" required placeholder="entrer le contenu"></textarea>
         <!-- POUR L'IMAGE, ON DEVRAIT PROPOSER UN UPLOAD => PLUS TARD... -->
@@ -9,9 +9,45 @@
         <!-- https://www.php.net/manual/fr/function.date.php -->
         <input type="text" name="datePublication" value="<?php echo date("Y-m-d H:i:s") ?>">
         <input type="text" name="categorie" required placeholder="entrez la catégorie">
+        <!--
+        <input type="text" style="display:none;" name="identifiantFormulaire" value="create" readonly>
+        -->
+        <input type="hidden" name="identifiantFormulaire" value="create">
+
         <button type="submit">PUBLIER L'ARTICLE</button>
         <div class="confirmation">
-            <?php require_once "php/controller/form-articles.php" ?>
+            <?php 
+$identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
+if ($identifiantFormulaire == "create")
+{
+    require "php/controller/form-articles.php"; 
+}        
+            ?>
         </div>
     </form>
 </section>
+
+
+<section>
+    <h2>FORMULAIRE DE DELETE</h2>
+    <form id="delete" action="" method="POST">
+        <input type="text" name="id" required placeholder="entrez id">
+        <!-- CODE BARRE TECHNIQUE POUR DISTINGUER LES FORMULAIRES -->
+        <input type="hidden" name="identifiantFormulaire" value="delete">
+        <button>SUPPRIMER LA LIGNE</button>
+        <div class="confirmation">
+        <?php 
+$identifiantFormulaire = $_REQUEST["identifiantFormulaire"] ?? "";
+if ($identifiantFormulaire == "delete")
+{
+    require "php/controller/form-articles.php"; 
+}        
+            ?>
+        </div>
+    </form>    
+</section>
+
+
+
+
+
