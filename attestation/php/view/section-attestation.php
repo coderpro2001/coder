15caa5f78fae66bc6e0ff5e4196929c78c7fad7a
LH <github@applh.com>
2020-03-17 13:47:52 +0100

    nouveau liveshare

--- attestation/php/view/section-attestation.php
+++ attestation/php/view/section-attestation.php
@@ -1,6 +1,10 @@
 
         <section>
             <h2>FORMULAIRE DE DECLARATION POUR ATTESTATION</h2>
-            <p>APRES LA PAUSE DEJEUNER... ;-p</p>
+            <form action="" method="POST">
+                <input type="text" name="nom" required placeholder="votre nom">
+                <input type="text" name="prenom" required placeholder="votre prenom">
+                <textarea name="adresse" cols="60" rows="6" required placeholder="votre adresse"></textarea>
+            </form>
         </section>
 
