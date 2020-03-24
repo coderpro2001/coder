62af3a8d417973fe3ca13e523c05d4020e117439
LH <github@applh.com>
2020-03-24 16:39:57 +0100

    ajout code montheme

---
+++ montheme/page-templates/template-contact.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Template Name: MON TEMPLATE CONTACT
+ */
+
+// DEBUG
+echo "IL FAUT CODER MON TEMPLATE CONTACT"; 
---
+++ montheme/page-templates/template-services.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * Template Name: MON TEMPLATE SERVICES
+ * 
+ * (attention: pas d'espace avant:)
+ * 
+ * ANNOTATION: 
+ *  COMMENTAIRE POUR LE LANGAGE
+ *  MAIS C'EST DU CODE POUR LE FRAMEWORK
+ */
+
+
+ // DEBUG
+echo "CECI SERA MON TEMPLATE POUR LA PAGE SERVICES"; 
