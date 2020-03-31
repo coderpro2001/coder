be06d7fe2d4bb16509e470e98d651d08996895b0
LH <github@applh.com>
2020-03-31 11:03:19 +0200

    ajout code revision-js

--- cms/php/class/Site.php
+++ cms/php/class/Site.php
@@ -5,7 +5,7 @@ class Site
     static function afficherPage ($nomPage)
     {
         require_once "php/view/header.php";
-        require_once "php/view/section-index.php";
+        require_once "php/view/section-$nomPage.php";
         require_once "php/view/footer.php";
     }
 }
